9b664d75d39cc86020c0046c4dcf7bcfced5c324
Mohamed <moh.medh.tem1@gmail.com>
2023-08-24 12:40:53 +0300

    spliting to section code and assignments

---
+++ Section 5 - Working with ExpressJs/Section-Code/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+const app = express();
+
+app.use('/add-product', (req, res, next) => {
+  res.send('<h1>The "Add Product" page');
+});
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hello from Express!</h1>');
+});
+
+app.listen(3000);
