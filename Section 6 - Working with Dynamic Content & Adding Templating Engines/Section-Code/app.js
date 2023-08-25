349b65aa42da4e029e052daaf21827a9031855cc
Mohamed <moh.medh.tem1@gmail.com>
2023-08-25 22:45:27 +0300

    sending dynamic html with pug

--- Section 6 - Working with Dynamic Content & Adding Templating Engines/Section-Code/app.js
+++ Section 6 - Working with Dynamic Content & Adding Templating Engines/Section-Code/app.js
@@ -5,6 +5,9 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -15,7 +18,7 @@ app.use('/admin', adminData.adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+  res.status(404).render('404', { pageTitle: 'Page not Found' });
 });
 
 app.listen(3000);
