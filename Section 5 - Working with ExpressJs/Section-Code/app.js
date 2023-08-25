7545abd5def11625846a9fcd4fce28278fa2c09a
Mohamed <moh.medh.tem1@gmail.com>
2023-08-25 11:24:36 +0300

    handle 404 req pages

--- Section 5 - Working with ExpressJs/Section-Code/app.js
+++ Section 5 - Working with ExpressJs/Section-Code/app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -12,7 +14,7 @@ app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).write('<h1>Page Not Found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
