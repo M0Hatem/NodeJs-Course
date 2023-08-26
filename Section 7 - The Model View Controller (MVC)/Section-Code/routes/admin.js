848e0d71c17a0d34535ee8c3a8a938b05e51a847
Mohamed <moh.medh.tem1@gmail.com>
2023-08-26 13:45:01 +0300

    initial commit

---
+++ Section 7 - The Model View Controller (MVC)/Section-Code/routes/admin.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+router.get('/add-product', (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: 'admin/add-product',
+  });
+});
+
+router.post('/add-product', (req, res) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.adminRoutes = router;
+exports.products = products;
