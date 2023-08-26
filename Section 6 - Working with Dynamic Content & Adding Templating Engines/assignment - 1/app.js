009ef525095e5b145fbeded7973722a7883217c9
Mohamed <moh.medh.tem1@gmail.com>
2023-08-26 13:43:05 +0300

    Assignment Done!

--- Section 6 - Working with Dynamic Content & Adding Templating Engines/assignment - 1/app.js
+++ Section 6 - Working with Dynamic Content & Adding Templating Engines/assignment - 1/app.js
@@ -1,11 +1,30 @@
-const path = require('path');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use('/', (req, res, next) => {
-  res.send('hello');
+const users = [];
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res, next) => {
+  res.render('index', { pageTitle: 'Add User' });
 });
 
-app.listen(3000);
+app.get('/users', (req, res, next) => {
+  res.render('users', {
+    pageTitle: 'User',
+    users: users,
+    hasUsers: users.length > 0,
+  });
+});
+
+app.post('/add-user', (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect('/users');
+});
+
+app.listen(3001);
