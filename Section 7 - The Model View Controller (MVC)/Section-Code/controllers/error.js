ec3eb79f0bf608da2fa5770f25151608c31cd750
Mohamed <moh.medh.tem1@gmail.com>
2023-08-27 23:44:15 +0300

    spliting routers to medules and controllers

---
+++ Section 7 - The Model View Controller (MVC)/Section-Code/controllers/error.js
@@ -0,0 +1,5 @@
+const path = require('path');
+
+exports.get404Page = (req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page not Found', path: path });
+};
--- Section 7 - The Model View Controller (MVC)/Section-Code/controllers/products.js
+++ Section 7 - The Model View Controller (MVC)/Section-Code/controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
   res.render('add-product', {
@@ -8,15 +8,19 @@ exports.getAddProduct = (req, res, next) => {
 };
 
 exports.postAddProduct = (req, res) => {
-  products.push({ title: req.body.title });
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
-  res.render('shop', {
-    prods: products,
-    pageTitle: 'Shop',
-    path: '/',
-    hasProducts: products.length > 0,
+  Product.fetchAll((products) => {
+    res.render('shop', {
+      prods: products,
+      pageTitle: 'Shop',
+      path: '/',
+      hasProducts: products.length > 0,
+    });
   });
+  // console.log(products);
 };
