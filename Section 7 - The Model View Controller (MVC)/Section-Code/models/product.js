a6a8536a55916c7452d508ad4bdded0164c69961
Mohamed <moh.medh.tem1@gmail.com>
2023-08-27 23:53:52 +0300

    working on  data

--- Section 7 - The Model View Controller (MVC)/Section-Code/models/product.js
+++ Section 7 - The Model View Controller (MVC)/Section-Code/models/product.js
@@ -3,30 +3,32 @@ const path = require('path');
 
 const rootDir = require('../util/path');
 
+const getData = function (cb) {
+  const p = path.join(rootDir, 'data', 'products.json');
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+  let products = [];
+};
+
 module.exports = class Product {
   constructor(t) {
     this.title = t;
   }
   save() {
-    const p = path.join(rootDir, 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getData((data) => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), (err) => {
         // console.log(err);
       });
     });
+    const p = path.join(rootDir, 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {});
   }
   static fetchAll(cb) {
-    const p = path.join(rootDir, 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getData(cb);
   }
 };
