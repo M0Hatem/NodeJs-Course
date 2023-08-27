75549881678af4a9e8693c06c245b06b3f29e9b9
Mohamed <moh.medh.tem1@gmail.com>
2023-08-27 17:51:51 +0300

    Adding products controller

--- Section 7 - The Model View Controller (MVC)/Section-Code/app.js
+++ Section 7 - The Model View Controller (MVC)/Section-Code/app.js
@@ -8,17 +8,17 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.adminRoutes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page not Found' });
+  res.status(404).render('404', { pageTitle: 'Page not Found', path: path });
 });
 
 app.listen(3000);
---
+++ Section 7 - The Model View Controller (MVC)/Section-Code/controllers/products.js
@@ -0,0 +1,22 @@
+const products = [];
+
+exports.getAddProduct = (req, res, next) => {
+  res.render('add-product', {
+    pageTitle: 'Add Product',
+    path: 'admin/add-product',
+  });
+};
+
+exports.postAddProduct = (req, res) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+};
+
+exports.getProducts = (req, res, next) => {
+  res.render('shop', {
+    prods: products,
+    pageTitle: 'Shop',
+    path: '/',
+    hasProducts: products.length > 0,
+  });
+};
