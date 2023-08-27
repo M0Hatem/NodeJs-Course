201997c99f492f7466fabfc744db3945578ea132
Mohamed <moh.medh.tem1@gmail.com>
2023-08-27 23:58:51 +0300

    inital commit

---
+++ Section 8 - Optional -  Enhancing the App/Section-Code/routes/admin.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const productsController = require('../controllers/products');
+
+const router = express.Router();
+
+router.get('/add-product', productsController.getAddProduct);
+
+router.post('/add-product', productsController.postAddProduct);
+
+module.exports = router;
