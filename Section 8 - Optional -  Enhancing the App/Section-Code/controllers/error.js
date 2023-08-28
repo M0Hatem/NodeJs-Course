058a5d995f5e30835a04e3317de5abcd1739bc8e
Mohamed <moh.medh.tem1@gmail.com>
2023-08-28 11:59:15 +0300

    skip to next Section =>

--- Section 8 - Optional -  Enhancing the App/Section-Code/controllers/error.js
+++ Section 8 - Optional -  Enhancing the App/Section-Code/controllers/error.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
-exports.get404Page = (req, res, next) => {
-  res.status(404).render('404', { pageTitle: 'Page not Found', path: path });
+exports.get404 = (req, res, next) => {
+  res.status(404).render('404', { pageTitle: 'Page Not Found', path: '/404' });
 };
--- Section 8 - Optional -  Enhancing the App/Section-Code/models/product.js
+++ Section 8 - Optional -  Enhancing the App/Section-Code/models/product.js
@@ -7,7 +7,7 @@ const p = path.join(
   'products.json'
 );
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
@@ -18,14 +18,17 @@ const getProductsFromFile = (cb) => {
 };
 
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
-    getProductsFromFile((products) => {
+    getProductsFromFile(products => {
       products.push(this);
-      fs.writeFile(p, JSON.stringify(products), (err) => {
+      fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
       });
     });
