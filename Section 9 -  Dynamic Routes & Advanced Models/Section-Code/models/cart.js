6f1b30328c25feef87c16c27d1591f3bad247d83
Mohamed <moh.medh.tem1@gmail.com>
2023-08-30 22:48:17 +0300

    intial commit for adding cart

---
+++ Section 9 -  Dynamic Routes & Advanced Models/Section-Code/models/cart.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+const path = require('path');
+const { exit } = require('process');
+
+const p = path.join(path.dirname(require.main.filename), 'data', 'cart.json');
+module.exports = class Cart {
+  static addProduct(id, productPrice) {
+    fs.readFile(p, (err, fileContent) => {
+      let cart = {
+        products: [],
+        totalPrice: 0,
+      };
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+      //find existing product
+      const existingProductIndex = cart.products.findIndex(
+        (prod) => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+      cart.totalPrice += productPrice;
+      fs.writeFile(p, JSON.stringify(cart), (err) => {
+        console.log(err);
+      });
+    });
+  }
+};
