5cbe7198c2c3c279e1e3e084d6e85bd3b4b3d480
Mohamed <moh.medh.tem1@gmail.com>
2023-11-08 11:57:03 +0200

    add multi products to cart with mongodb
    Signed-off-by: Mohamed <moh.medh.tem1@gmail.com>

--- Section 12 - Working with NoSQL & Using MongoDB/Section-Code/models/user.js
+++ Section 12 - Working with NoSQL & Using MongoDB/Section-Code/models/user.js
@@ -16,10 +16,25 @@ class User {
   }
 
   addToCart(product) {
+    const cartProductIndex = this.cart.items.findIndex((cp) => {
+      return cp.productId.toString() === product._id.toString();
+    });
+    let newQuantity;
+    const updatedCartItems = [...this.cart.items];
+    if (cartProductIndex >= 0) {
+      newQuantity = this.cart.items[cartProductIndex].quantity + 1;
+      updatedCartItems[cartProductIndex].quantity = newQuantity;
+    } else {
+      updatedCartItems.push({
+        productId: new ObjectId(product._id),
+        quantity: newQuantity,
+      });
+    }
     const updatedCart = {
-      item: [{ productId: new ObjectId(product._id), quantity: 1 }],
+      items: updatedCartItems,
     };
     const db = getDb();
+
     return db
       .collection("users")
       .updateOne(
