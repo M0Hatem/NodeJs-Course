388afc4249332fd0b2d67d9f05158c41dd4bd048
Mohamed <moh.medh.tem1@gmail.com>
2023-11-12 11:12:36 +0200

    initial commit

--- Section - 15 Adding Authentication/controllers/auth.js
+++ Section - 15 Adding Authentication/controllers/auth.js
@@ -1,24 +1,39 @@
-const User = require("../models/user");
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-    res.render('auth/login',
-        {path: '/login',
-        pageTitle: 'Login',
-        isAuthenticated:  req.session.loggedin
-        });
-};
-exports.postLogin = (req, res, next) => {
-    User.findById('65505eccb307e3b65348b589').then((user)=>{
-    req.session.loggedin=true;
-    req.session.user =user
-    req.session.save(()=>{
-    res.redirect("/")
-    })
-    })
-    .catch(err=>console.log(err))
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false
+  });
 };
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    isAuthenticated: false
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('5bab316ce0a7c75f783cb8a8')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      req.session.save(err => {
+        console.log(err);
+        res.redirect('/');
+      });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {};
+
 exports.postLogout = (req, res, next) => {
-     req.session.destroy(()=>{
-         res.redirect('/')
-     })
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
