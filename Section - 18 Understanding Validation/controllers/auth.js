a7138a6cbed48c68943bcbded54dfe10fd7b461d
Mohamed <moh.medh.tem1@gmail.com>
2023-11-13 22:53:59 +0200

    added validation on email with expvalidator

--- Section - 18 Understanding Validation/controllers/auth.js
+++ Section - 18 Understanding Validation/controllers/auth.js
@@ -1,6 +1,7 @@
 const crypto = require("crypto");
 
 const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
 
 const User = require("../models/user");
 
@@ -62,6 +63,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "Signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   User.findOne({ email: email })
     .then((userData) => {
       if (userData) {
