89e563f49c44d3b443a521a0949bc5d1560a53cb
Mohamed <moh.medh.tem1@gmail.com>
2023-11-13 23:13:25 +0200

    added validation on Password with expvalidator

--- Section - 18 Understanding Validation/routes/auth.js
+++ Section - 18 Understanding Validation/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { check } = require("express-validator");
+const { check, body } = require("express-validator");
 
 const authController = require("../controllers/auth");
 
@@ -13,7 +13,21 @@ router.post("/login", authController.postLogin);
 
 router.post(
   "/signup",
-  check("email").isEmail().withMessage("Please Enter a Valid Email."),
+  [
+    check("email").isEmail().withMessage("Please Enter a Valid Email."),
+    body(
+      "password",
+      "Please enter a password with only numbers and text and at least 5 characters."
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((val, { req }) => {
+      if (val !== req.body.password) {
+        throw new Error("Passwords have to match!");
+      }
+      return true;
+    }),
+  ],
   authController.postSignup
 );
 
