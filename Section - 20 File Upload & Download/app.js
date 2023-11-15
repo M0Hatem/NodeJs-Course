63aa4f3175897e7adaf5e2e306376a28f660c1c2
Mohamed <moh.medh.tem1@gmail.com>
2023-11-15 11:30:10 +0200

    filtering data with mimetype

--- Section - 20 File Upload & Download/app.js
+++ Section - 20 File Upload & Download/app.js
@@ -32,6 +32,18 @@ const fileStorage = multer.diskStorage({
   },
 });
 
+const fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === "image/png" ||
+    file.mimetype === "jpg" ||
+    file.mimetype === "jpeg"
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
+
 app.set("view engine", "ejs");
 app.set("views", "views");
 
@@ -40,7 +52,9 @@ const shopRoutes = require("./routes/shop");
 const authRoutes = require("./routes/auth");
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(multer({ storage: fileStorage }).single("image"));
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single("image")
+);
 app.use(express.static(path.join(__dirname, "public")));
 app.use(
   session({
