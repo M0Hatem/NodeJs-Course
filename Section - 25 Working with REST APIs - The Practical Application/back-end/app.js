2dd1612873b02d2db4d140f2073f568c54f9b88f
Mohamed <moh.medh.tem1@gmail.com>
2023-11-18 18:20:48 +0200

    handling errors

--- Section - 25 Working with REST APIs - The Practical Application/back-end/app.js
+++ Section - 25 Working with REST APIs - The Practical Application/back-end/app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
@@ -7,6 +9,7 @@ const feedRouter = require("./routes/feed");
 const app = express();
 
 app.use(bodyParser.json());
+app.use("images", express.static(path.join(__dirname, "images")));
 
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
@@ -16,6 +19,12 @@ app.use((req, res, next) => {
 });
 
 app.use("/feed", feedRouter);
+
+app.use((error, req, res, next) => {
+  const status = error.statusCode || 500;
+  const message = error.message;
+  res.status(status).json({ message: message });
+});
 
 mongoose
   .connect(
--- Section - 25 Working with REST APIs - The Practical Application/back-end/controllers/feed.js
+++ Section - 25 Working with REST APIs - The Practical Application/back-end/controllers/feed.js
@@ -22,10 +22,9 @@ exports.getPosts = (req, res, next) => {
 exports.creatPost = (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    return res.status(422).json({
-      message: "validation failed ,entered data is not correct",
-      errors: errors.array(),
-    });
+    const error = new Error("validation failed ,entered data is not correct");
+    error.statusCode = 422;
+    throw error;
   }
   const title = req.body.title;
   const content = req.body.content;
@@ -45,5 +44,10 @@ exports.creatPost = (req, res, next) => {
         post: result,
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      if (!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
 };
