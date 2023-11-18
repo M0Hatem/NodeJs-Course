084ed7e3d6e8dbc4b4d3dab1fc544f7b04b52fa4
Mohamed <moh.medh.tem1@gmail.com>
2023-11-18 18:06:35 +0200

    storing posts in db

--- Section - 25 Working with REST APIs - The Practical Application/back-end/controllers/feed.js
+++ Section - 25 Working with REST APIs - The Practical Application/back-end/controllers/feed.js
@@ -1,27 +1,49 @@
-const { validationResult }= require('express-validator')
+const { validationResult } = require("express-validator");
 
-exports.getPosts = (req,res,next)=>{
-    res.status(200).json({
-        posts:[{_id:"1",title:"First Post",content:'this is the first post!',imageUrl:"images/nigaCat.jpeg",creator:{
-            name:"M16"
-            },
-        createdAt: new Date()}]
-    })
-}
+const Post = require("../models/post");
 
-exports.creatPost = (req,res,next)=>{
-    const errors = validationResult(req)
-    if (!errors.isEmpty())
-    {
-     return res.status(422).json({message:'validation failed ,entered data is not correct',errors:errors.array()})
-    }
-    const title = req.body.title;
-    const content = req.body.content;
+exports.getPosts = (req, res, next) => {
+  res.status(200).json({
+    posts: [
+      {
+        _id: "1",
+        title: "First Post",
+        content: "this is the first post!",
+        imageUrl: "images/nigaCat.jpeg",
+        creator: {
+          name: "M16",
+        },
+        createdAt: new Date(),
+      },
+    ],
+  });
+};
 
-    res.status(201).json({
+exports.creatPost = (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(422).json({
+      message: "validation failed ,entered data is not correct",
+      errors: errors.array(),
+    });
+  }
+  const title = req.body.title;
+  const content = req.body.content;
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: "images/duck",
+    creator: {
+      name: "M16",
+    },
+  });
+  post
+    .save()
+    .then((result) => {
+      res.status(201).json({
         message: "post created successfully",
-        post:{_id:new Date().toISOString(),title:title,content:content,creator:{
-            name:"M16"
-            },createdAt: new Date()}
+        post: result,
+      });
     })
-}
+    .catch((err) => console.log(err));
+};
