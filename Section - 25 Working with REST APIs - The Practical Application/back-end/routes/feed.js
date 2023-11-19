2c234504ba43c28e7296beab0bbb51d64d31ba39
Mohamed <moh.medh.tem1@gmail.com>
2023-11-19 12:24:44 +0200

    using and validating the jwt

--- Section - 25 Working with REST APIs - The Practical Application/back-end/routes/feed.js
+++ Section - 25 Working with REST APIs - The Practical Application/back-end/routes/feed.js
@@ -2,10 +2,11 @@ const express = require("express");
 const { body } = require("express-validator");
 
 const feedController = require("../controllers/feed");
+const isAuth = require("../middleware/is-auth");
 
 const router = express.Router();
 
-router.get("/posts", feedController.getPosts);
+router.get("/posts", isAuth, feedController.getPosts);
 
 router.post(
   "/post",
