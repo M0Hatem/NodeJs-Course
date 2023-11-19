ffaf561790d9bb5e7a4847ea3953ec88042df8cb
Mohamed <moh.medh.tem1@gmail.com>
2023-11-19 13:31:05 +0200

    connecting posts&user

--- Section - 25 Working with REST APIs - The Practical Application/back-end/controllers/feed.js
+++ Section - 25 Working with REST APIs - The Practical Application/back-end/controllers/feed.js
@@ -4,6 +4,7 @@ const path = require("path");
 const { validationResult } = require("express-validator");
 
 const Post = require("../models/post");
+const User = require("../models/user");
 
 exports.getPosts = (req, res, next) => {
   const currentPage = req.query.page || 1;
@@ -51,16 +52,24 @@ exports.creatPost = (req, res, next) => {
     title: title,
     content: content,
     imageUrl: imageUrl,
-    creator: {
-      name: "M16",
-    },
+    creator: req.userId,
   });
+  let creator;
   post
     .save()
     .then((result) => {
+      return User.findById(req.userId);
+    })
+    .then((user) => {
+      creator = user;
+      user.posts.push(post);
+      return user.save();
+    })
+    .then((result) => {
       res.status(201).json({
         message: "post created successfully",
-        post: result,
+        post: post,
+        creator: { _id: creator._id, name: creator.name },
       });
     })
     .catch((err) => {
